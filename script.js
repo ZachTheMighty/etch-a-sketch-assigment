e2c9b041123e32784aba3533994f7b415989035c
ZachTheMighty <promoted66@gmail.com>
2026-04-01 07:26:14 +0000

    Added a 16x16 grid of squares

---
+++ script.js
@@ -0,0 +1,8 @@
+let container = document.querySelector(".container");
+let square;
+
+for (let i = 1; i < 257; i++) {
+  square = document.createElement("div");
+  square.textContent = i;
+  container.appendChild(square);
+}
