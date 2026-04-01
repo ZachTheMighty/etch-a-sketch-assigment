bf9d60229d5598420621df4d1bd648acd01e43da
ZachTheMighty <promoted66@gmail.com>
2026-04-01 16:47:17 +0000

    Added the actual drawing logic

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ for (let i = 1; i < 257; i++) {
   square = document.createElement("div");
   container.appendChild(square);
 }
+
+let squares = document.querySelectorAll(".container div");
+
+squares.forEach((square) => {
+  square.addEventListener("mouseenter", () =>
+    square.classList.toggle("orange"),
+  );
+  square.addEventListener("mouseleave", () =>
+    square.classList.toggle("fade"),
+  );
+});
