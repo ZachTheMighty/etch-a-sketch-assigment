2e4ccc7ee3b794bb79fbda3043c86c4267bbaf27
ZachTheMighty <promoted66@gmail.com>
2026-04-02 16:44:05 +0000

    Added a button for a flexibly changing grid size
    Added a button for flexibly changing grid size
    
    Added a button for flexibly changing grid size

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let container = document.querySelector(".container");
+let container = document.querySelector("#container");
 const containerDimensions = 500;
-let square;
+const button = document.querySelector("button");
+let square, squares;
 
 function createGrid(numberOfSquares) {
+  container = document.createElement("div");
+  document.querySelector("body").append(container);
+  if (isNaN(numberOfSquares) || numberOfSquares === 0) return;
+
   for (let i = 0; i < Math.pow(numberOfSquares, 2); i++) {
     square = document.createElement("div");
     square.classList.add("square");
@@ -11,15 +16,35 @@ function createGrid(numberOfSquares) {
     square.style.height = `${dimension}px`;
     container.appendChild(square);
   }
+  container.classList.toggle("container");
+  container.classList.toggle("active");
+  leaveTrail();
 }
 
-createGrid(10);
+function leaveTrail() {
+  squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseenter", () =>
+      square.classList.toggle("orange"),
+    );
 
-let squares = document.querySelectorAll(".square");
+    square.addEventListener("mouseleave", () =>
+      square.classList.toggle("fade"),
+    );
+  });
+}
 
-squares.forEach((square) => {
-  square.addEventListener("mouseenter", () =>
-    square.classList.toggle("orange"),
-  );
-  square.addEventListener("mouseleave", () => square.classList.toggle("fade"));
+function clearGrid() {
+  container.remove();
+  container.classList.toggle("active");
+}
+
+button.addEventListener("click", () => {
+  let number = 0;
+  do {
+    number = Number(prompt("Enter a number in the interval [1, 100)"));
+  } while (number !== 0 && (number < 1 || number > 99));
+
+  if (container.classList.contains("active")) clearGrid();
+  createGrid(number);
 });
